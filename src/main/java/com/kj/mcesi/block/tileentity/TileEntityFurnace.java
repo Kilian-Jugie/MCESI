f7451f6c8c1e0783b47fded89544e856b9bd424a
kilian3309 <kiki.jugie@gmail.com>
2019-10-31 17:40:47 +0100

    Fixed: furnace don't need non-empty result to work Added: furnace update is server side only

--- src/main/java/com/kj/mcesi/block/tileentity/TileEntityFurnace.java
+++ src/main/java/com/kj/mcesi/block/tileentity/TileEntityFurnace.java
@@ -26,13 +26,13 @@ import net.minecraft.util.ReportedException;
 public class TileEntityFurnace extends KTileEntityMachine {
 	private int m_ProcessTicks = 0;
 	private boolean m_IsInProcess = false;
-	
+
 	private static final int SLOT_INPUT = 0;
 	private static final int SLOT_OUTPUT = 2;
-	
+
 	private MachineCraftRegistry craftRegistry = new MachineCraftRegistry();
 	private MachineCraft m_CurrentCraft;
-	
+
 	public TileEntityFurnace() {
 		super("tfurnace");
 		craftRegistry.addCraft(
@@ -44,55 +44,57 @@ public class TileEntityFurnace extends KTileEntityMachine {
 	public void update() {
 		super.update();
 	}
-	
+
 	private boolean canStartProcess(ItemStack output) {
-		
-		return ItemStack.areItemsEqual(getItems().get(SLOT_OUTPUT), output) &&
-				getItems().get(SLOT_OUTPUT).getCount()<getInventoryStackLimit();
+
+		return getItems().get(SLOT_OUTPUT).isEmpty() || (ItemStack.areItemsEqual(getItems().get(SLOT_OUTPUT), output) &&
+				getItems().get(SLOT_OUTPUT).getCount()<getInventoryStackLimit());
 	}
-	
+
 	private void updateProcess() {
-		if(m_IsInProcess) {
-			if(m_ProcessTicks < 0) {
-				MCESI.logger.info("PROCESS ENDED !");
-				m_ProcessTicks = 0;
-				m_IsInProcess = false;
-				
-				//getItems().set(SLOT_OUTPUT, new ItemStack((Block)ModBlocks.getBlocks().get(0)));
-				if(getItems().get(SLOT_OUTPUT).isEmpty()) {
-					getItems().set(SLOT_OUTPUT, m_CurrentCraft.getOutputs().get(0));
+		if(!this.world.isRemote) {
+			if(m_IsInProcess) {
+				if(m_ProcessTicks < 0) {
+					MCESI.logger.info("PROCESS ENDED !");
+					m_ProcessTicks = 0;
+					m_IsInProcess = false;
+
+					//getItems().set(SLOT_OUTPUT, new ItemStack((Block)ModBlocks.getBlocks().get(0)));
+					if(getItems().get(SLOT_OUTPUT).isEmpty()) {
+						getItems().set(SLOT_OUTPUT, m_CurrentCraft.getOutputs().get(0));
+					}
+					else {
+						getItems().get(SLOT_OUTPUT).setCount(getItems().get(SLOT_OUTPUT).getCount()+1);
+					}
 				}
 				else {
-					getItems().get(SLOT_OUTPUT).setCount(getItems().get(SLOT_OUTPUT).getCount()+1);
+					MCESI.logger.info("PROCESSING State: "+m_ProcessTicks);
+					m_ProcessTicks--;
 				}
 			}
-			else {
-				MCESI.logger.info("PROCESSING State: "+m_ProcessTicks);
-				m_ProcessTicks--;
-			}
-		}
-		else if(!getItems().get(SLOT_INPUT).isEmpty()) {
-			m_CurrentCraft = craftRegistry.getCraftForInput(getItems().get(SLOT_INPUT));
-			if(m_CurrentCraft != null&&canStartProcess(m_CurrentCraft.getOutputs().get(0))) {
-				m_ProcessTicks = m_CurrentCraft.getProcessTime();
-				m_IsInProcess = true;
-				getItems().get(SLOT_INPUT).shrink(1);
-			}
-			
-			/*if(ItemStack.areItemsEqual(getItems().get(0), new ItemStack((Block)ModBlocks.getBlocks().get(0)))) {
+			else if(!getItems().get(SLOT_INPUT).isEmpty()) {
+				m_CurrentCraft = craftRegistry.getCraftForInput(getItems().get(SLOT_INPUT));
+				if(m_CurrentCraft != null&&canStartProcess(m_CurrentCraft.getOutputs().get(0))) {
+					m_ProcessTicks = m_CurrentCraft.getProcessTime();
+					m_IsInProcess = true;
+					getItems().get(SLOT_INPUT).shrink(1);
+				}
+
+				/*if(ItemStack.areItemsEqual(getItems().get(0), new ItemStack((Block)ModBlocks.getBlocks().get(0)))) {
 				MCESI.logger.info("EQUALS !");
 				m_ProcessTicks = 20;
 				m_IsInProcess = true;
 				getItems().get(SLOT_INPUT).shrink(1);
 				//getItems().set(SLOT_INPUT, );
 				//getItems().set(SLOT_INPUT, ItemStack.EMPTY);
-				
+
 			}*/
+			}
 		}
-		
-		
+
+
 	}
-	
+
 	@Override
 	public void fixedUpdate() {
 		updateProcess();
